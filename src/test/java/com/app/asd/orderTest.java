1c48e6a5098c41b9971a18e2241e395274c23ae3
ShawnShens <44152880+ShawnShens@users.noreply.github.com>
2020-09-26 20:46:32 +1000

    Add files via upload

---
+++ src/test/java/com/app/asd/orderTest.java
@@ -0,0 +1,38 @@
+import com.app.asd.Model.*;
+import com.app.asd.Utils.dbConnect;
+import com.mongodb.*;
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+import org.junit.Before;
+import org.junit.Test;
+
+public class orderTest {
+    private MongoCollection<Document> collection = null;
+    private MongoClient mongoClient = null;
+    private MongoDatabase db = null;
+
+    @Before
+    public void start()
+    {
+        mongoClient = new MongoClient(new MongoClientURI("mongodb+srv://wenhuili:lwh971213@cluster0.bgi8l.mongodb.net/ASD?retryWrites=true&w=majority"));
+        db = mongoClient.getDatabase("ASD");
+        collection = dbConnect.getMongoCollection("Order");
+    }
+
+    @Test
+    public void testGetCollection() throws Exception
+    {
+        collection = dbConnect.getMongoCollection("Order");
+    }
+
+    @Test
+    public void testInsertOrder()
+    {
+        Document document = new Document("orderID", "100001");
+        document.append("orderComment","junit Test");
+        dbConnect.insertOneDocument("Order", document);
+    }
+}
