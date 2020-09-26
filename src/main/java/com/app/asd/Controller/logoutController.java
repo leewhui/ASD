e45cb4fdd9c786864e05640c36e314ee1a9d14b6
leewhui <44152475+leewhui@users.noreply.github.com>
2020-09-26 19:27:19 +0800

    Update logoutController.java

--- src/main/java/com/app/asd/Controller/logoutController.java
+++ src/main/java/com/app/asd/Controller/logoutController.java
@@ -11,6 +11,7 @@ public class logoutController extends HttpServlet
 {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // when the user logout, destory the session
         HttpSession session = req.getSession();
         session.removeAttribute("currentUser");
         resp.sendRedirect("main.jsp");
