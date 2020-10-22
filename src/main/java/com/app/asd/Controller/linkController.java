216236ab736d62f7f9368b898d077b0657fe55d1
ShawnShens <44152880+ShawnShens@users.noreply.github.com>
2020-10-23 00:47:58 +1100

    Update linkController.java

--- src/main/java/com/app/asd/Controller/linkController.java
+++ src/main/java/com/app/asd/Controller/linkController.java
@@ -12,6 +12,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -41,7 +42,7 @@ public class linkController extends HttpServlet {
             
             
             session.setAttribute("error","Card number does not exist.");
-            resp.sendRedirect( page )
+            resp.sendRedirect( page );
             
                 
             //resp.sendRedirect( page + "?error=Card number does not exist.");
