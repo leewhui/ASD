ddc8e76c2302a6397d78f1f1a4fba59f86640e4c
simonnosleep <49275843+simonnosleep@users.noreply.github.com>
2020-10-22 21:22:42 +1100

    Add files via upload

--- src/main/java/com/app/asd/Controller/editController.java
+++ src/main/java/com/app/asd/Controller/editController.java
@@ -23,8 +23,8 @@ public class editController extends HttpServlet {
         User user = (User)req.getSession().getAttribute("currentUser");
         if(user != null) {
             BasicDBObject obj = new BasicDBObject("userEmail", user.getEmail());
-            MongoCursor<Document> resultList = dbConnect.queryCursor("Card", obj);
-            List<Card> cards = new ArrayList<>();
+            MongoCursor<Document> resultList = dbConnect.queryCursor("Card", obj);//query 'Card' from MongoDB
+            List<Card> cards = new ArrayList<>();//get cards list
             Gson gson = new Gson();
             while(resultList.hasNext()){
                 cards.add(gson.fromJson(resultList.next().toJson(), Card.class));
@@ -38,22 +38,35 @@ public class editController extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        // Set the user infomation
+        // Set the user information
         User user = (User)req.getSession().getAttribute("currentUser");
-        user.setFirst_name(req.getParameter("firstName"));
-        user.setLast_name(req.getParameter("lastName"));
-        user.setDob(req.getParameter("dob"));
-        user.setEmail(req.getParameter("email"));
-        user.setPassword(req.getParameter("password"));
-        user.setGender(req.getParameter("gender"));
-        // Save to MongoDB.
-        if(dbConnect.updateOneUser(user).equals("success")) {
-            req.setAttribute("success", true);
+        if(user != null) {
+            BasicDBObject obj = new BasicDBObject("userEmail", user.getEmail());
+            MongoCursor<Document> resultList = dbConnect.queryCursor("Card", obj);//query 'Card' from MongoDB
+            //get cards list
+            List<Card> cards = new ArrayList<>();
+            Gson gson = new Gson();
+            //set attributes
+            while(resultList.hasNext()){
+                cards.add(gson.fromJson(resultList.next().toJson(), Card.class));
+                user.setFirst_name(req.getParameter("firstName"));
+                user.setLast_name(req.getParameter("lastName"));
+                user.setDob(req.getParameter("dob"));
+                user.setPassword(req.getParameter("password"));
+                user.setGender(req.getParameter("gender"));
+            }
+            // Save to MongoDB.
+            if(dbConnect.updateOneUser(user).equals("success")) {
+                req.setAttribute("success", true);
+            } else {
+                req.setAttribute("success", false);
+            }
+            req.setAttribute("cards", cards);
+            req.getRequestDispatcher("userInfo.jsp").include(req, resp);
         } else {
-            req.setAttribute("success", false);
+            resp.sendRedirect("login.jsp");
         }
-        req.setAttribute("userInfo", user);
-        req.getRequestDispatcher("userInfo.jsp").include(req, resp);
+
     }
 
 }
--- src/main/java/com/app/asd/Controller/linkController.java
+++ src/main/java/com/app/asd/Controller/linkController.java
@@ -24,17 +24,17 @@ public class linkController extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //action : 'link' and 'unlink'
         String action = req.getParameter("action");
         String cardNumber = req.getParameter("cardNumber");
         User user = (User)req.getSession().getAttribute("currentUser");
-
         BasicDBObject obj = new BasicDBObject("opalCardNumber", cardNumber);
         MongoCursor<Document> resultList = dbConnect.queryCursor("Card", obj);
         Card card = null;
         Gson gson = new Gson();
         if(resultList.hasNext()){
             card = gson.fromJson(resultList.next().toJson(), Card.class);
-        } else {
+        } else {//if action equals link, then the page=linkCard.jsp,
             String page = action.equals("link")?"linkCard.jsp":"unlinkCard.jsp";
             resp.sendRedirect( page + "?error=Card number does not exist.");
             return;
@@ -43,17 +43,18 @@ public class linkController extends HttpServlet {
         Document filter = new Document();
         filter.append("opalCardNumber", cardNumber);
         Document update = new Document();
-        if(action.equals("link")){
+        if(action.equals("link")){//if user click 'link' button
+            // to judge whether the card have been linked based on userEmail of card
             if(card.getUserEmail() == null){
                 update.append("$set", new Document("userEmail", user.getEmail()));
-            } else {
+            } else {//link itself
                 resp.sendRedirect("linkCard.jsp?error=The card has already been linked.");
                 return;
             }
-        } else {
-            if(user.getEmail().equals(card.getUserEmail())){
+        } else {//if user click 'unlink' button
+            if(user.getEmail().equals(card.getUserEmail())){//if the current user email equals the target card of user email
                 update.append("$set", new Document("userEmail", null));
-            } else {
+            } else {// if the target card of user email not equal the current user// if the card not yours but exists in MongoDB
                 resp.sendRedirect("unlinkCard.jsp?error=Cannot unlink a card that does not belong to you.");
                 return;
             }
