ab619cb704542b0b4659e6511482fd86c2cf8ec8
ShawnShens <44152880+ShawnShens@users.noreply.github.com>
2020-10-24 01:55:29 +1100

    Update cardStatusServlet.java

--- src/main/java/com/app/asd/Controller/cardStatusServlet.java
+++ src/main/java/com/app/asd/Controller/cardStatusServlet.java
@@ -43,6 +43,6 @@ public class cardStatusServlet extends HttpServlet {
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+         doPost(request, response);
     }
 }
